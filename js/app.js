fc85bb204adb7214052d8a75cff7b96e8fd36519
Piotr Celigowski <piotrcel2@gmail.com>
2016-11-06 23:22:15 +0100

    base

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
     
     
-    console.log('cześć!');
+    var snakelength = 3;
+    var points = 0;
+    var direction = 1; // 1-up, 2-right, 3-down, 4-left
+    
+    
+    console.log(snakelength);
     
     
     
