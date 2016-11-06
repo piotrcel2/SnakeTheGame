b905f0ac0f613027ff1f1825e9f9d6e1138730a3
Piotr Celigowski <piotrcel2@gmail.com>
2016-11-07 00:25:42 +0100

    base2

--- js/app.js
+++ js/app.js
@@ -1,13 +1,83 @@
 document.addEventListener("DOMContentLoaded", function(){
     
+    var errorbox = document.getElementById("errorbox");
+    var startbutton = document.getElementById("startbutton");
+    var levelbuttons = document.getElementsByClassName("lvl");
+    var infobox = document.getElementById("infobox");
     
     var snakelength = 3;
     var points = 0;
     var direction = 1; // 1-up, 2-right, 3-down, 4-left
+    var gamestart = false;
+    var level = 0;
+    var head = [8, 12];
+    var tail = [8, 14];
+    
+    var rows = [];
     
     
-    console.log(snakelength);
+    errorbox.classList.add("hidden");                       // schowanie errorboxa dla błędnego startu
     
+    
+    startbutton.addEventListener("click", function(e){      // kliknięcie start!
+        
+        if (level == 0){
+            
+            errorbox.classList.remove("hidden");
+            
+        } else {
+            
+            errorbox.classList.add("hidden");
+            infobox.classList.add("hidden");
+            console.log(level); 
+        }
+        
+        
+        
+        
+    })
+    
+    
+    for (var i=0; i<levelbuttons.length; i++){                  // pętla dla przycisków leveli
+        
+        levelbuttons[i].addEventListener("click", function(e){
+        
+            for (var i=0; i<levelbuttons.length; i++){
+                
+                levelbuttons[i].style.color = "black";          // nadanie każdemu elementowi domyślnego koloru, na wypadek gdyby już wcześniej był wybrany
+                
+            }
+            
+            this.style.color = "crimson";                       // kolorowanie wybranego levela
+            
+            level = this.dataset.lvl;                           // ustanowienie levela
+            console.log("wybierasz level " + this.dataset.lvl); 
+            
+        });
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    function resetboard(){
+        
+        
+    }
+    
+    
+    
+    
+    
+    function drawasnake(){
+        
+        
+    };
     
     
 });
